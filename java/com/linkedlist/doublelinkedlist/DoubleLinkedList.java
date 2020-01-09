67d9d93eff63cfe7f5dffc1831926a1d84bfcde1
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-08 19:29:07 -0500

    [Feature] Finished Double Linked List with all their implementations

--- java/com/linkedlist/doublelinkedlist/DoubleLinkedList.java
+++ java/com/linkedlist/doublelinkedlist/DoubleLinkedList.java
@@ -97,4 +97,68 @@ public class DoubleLinkedList {
 		}
 		System.out.println("null");
 	}
+	
+	public void deleteNode(int index) {		
+		if (!existsLinkedList()) {
+			System.out.println("The Linked List does not exists.");
+			return;
+		} else if (index >= size || index < 0) {
+			System.out.println("The index " + index + " is out of range.");
+			return;
+		} else if (index == 0) {
+			head = head.getNext();
+			head.setPrev(null);
+			
+			setSize(getSize() - 1);
+			
+			if (getSize() == 0) {
+				tail = null;
+			}
+		} else if (index == getSize() - 1) {
+			tail = tail.getPrev();
+			tail.setNext(null);
+		} else {
+			DoubleNode node = head;
+			
+			int count = 0;
+			while (count < index - 1) {
+				node = node.getNext();
+				count++;
+			}
+			node.getNext().getNext().setPrev(node);
+			node.setNext(node.getNext().getNext());
+		}
+		
+		setSize(getSize() - 1);
+		System.out.println("The node of index " + index + " has been deleted");
+	}
+	
+	public void deleteLinkedList() {
+		head = null;
+		tail = null;
+		size = 0;
+		System.out.println("The Single Linked List has been deleted.");
+	}
+	
+	public void printNode(int index) {
+		if (!existsLinkedList()) {
+			System.out.println("The Linked List does not exists.");
+		} else if (index >= size || index < 0) {
+			System.out.println("The index " + index + " is out of range.");
+		} else if (index == 0) {
+			System.out.println("This is the node of value " + head.getValue() + " and index " + index + ".");
+		} else if (index == size - 1) {
+			System.out.println("This is the node of value " + tail.getValue() + " and index " + index + ".");
+		} else {
+			DoubleNode temp = head;
+			int count = 0;
+			
+			while (count < index) {
+				temp = temp.getNext();
+				count++;
+			}
+			
+			System.out.println("This is the node of value " + temp.getValue() + " and index " + index + ".");
+		}
+	}
 }
--- java/com/linkedlist/doublelinkedlist/Main.java
+++ java/com/linkedlist/doublelinkedlist/Main.java
@@ -16,6 +16,31 @@ public class Main {
 		newList.insertInLinkedList(8);
 		
 		newList.printDoubleLinkedList();
-	}
+		
+		System.out.println("Size: " + newList.getSize());
+		
+		System.out.printf("===========================================================================\n\n");
+		
+		newList.insertInLinkedList(99999, 2);
+		
+		newList.insertInLinkedList(99999, 4);
+		
+		newList.printDoubleLinkedList();
+		
+		System.out.println("Size: " + newList.getSize());
+		
+		System.out.printf("===========================================================================\n\n");
+		
+		newList.printNode(4);
+		
+		newList.deleteNode(3);
 
+		newList.deleteNode(5);
+		
+		newList.printDoubleLinkedList();
+
+		System.out.println("Size: " + newList.getSize());
+		
+		System.out.printf("===========================================================================\n\n");
+	}
 }
